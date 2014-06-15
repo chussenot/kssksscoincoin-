c489aa8f010ee7b253695b3d7e34a7b0fc921c68
chussenot <chussenot@gmail.com>
2014-06-15 06:52:58 +0200

    Implement pledge

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,4 +1,5 @@
-
+# @@bitcoind method
+# https://blockchain.info/api/json_rpc_api
 
 class API < Grape::API
   format :json
@@ -6,11 +7,18 @@ class API < Grape::API
 	  'pong'
 	end
 
-  post :option do
-    {payload: @@bitcoind.listtransactions()}
+  post :pledge do
+    txid            = params[:transaction_id]
+    vout            = params[:vout].to_i
+    bitcoin_address = params[:bitcoin_address]
+    pledge_amount   = params[:pledge_amount].to_f
+    tx_input  = [{txid: txid ,vout: 0}]
+    tx_output = { "#{bitcoin_address}" => pledge_amount}
+    @@bitcoind.decoderawtransaction @@bitcoind.createrawtransaction(tx_input, tx_output)
   end
 
   get :transactions do
-    {payload: @@bitcoind.listtransactions()}
+    @@bitcoind.listtransactions()
   end
+  
 end
--- app/assets/javascripts/ksskss.coffee
+++ app/assets/javascripts/ksskss.coffee
@@ -10,10 +10,12 @@ window.KssKss.Application = class Application
     $('.ui.button.add_option').click ->
       $(".ui.modal.add_option").modal('show')
     $('.ui.button.request_option').click ->
-    	$.post("api/option",
-        name: "John"
-        time: "2pm"
-      ).done (data) ->
-        console.log data
+    	$.post("api/pledge",
+        transaction_id: "e3ba2ac29c70b3c5183adf0ca4ef9d7578ff0db4d857ffc94a7ca8c105a7fca8"
+        vout: 0
+        bitcoin_address: "mjADUJra6U1jDsdHXZ6JwAU4iaVKMzaXi7"
+        pledge_amount: 0.34113
+      ).done (transaction) ->
+        console.log transaction
 
 
